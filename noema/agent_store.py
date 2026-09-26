54e5853844e6f7270b8a101fa2a6008d6d802425
Builder Of Things <gryszzz6@gmail.com>
2026-09-26 00:28:04 -0400

    cognition: persist runtime cognition state

--- noema/agent_store.py
+++ noema/agent_store.py
@@ -106,6 +106,12 @@ class AgentStore:
                 evm_wallet=AgentConnectionState(**last_cycle_raw["evm_wallet"]),
                 radar_markets=int(last_cycle_raw["radar_markets"]),
                 economic_state=str(last_cycle_raw["economic_state"]),
+                cognition=AgentConnectionState(
+                    **last_cycle_raw.get(
+                        "cognition",
+                        {"status": "unconfigured", "detail": None},
+                    )
+                ),
                 note=last_cycle_raw.get("note"),
             )
 
